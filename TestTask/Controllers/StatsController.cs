ab9adffe06d6b2ac2df1b43a92dc13b6f4238935
Nika <92479422+nikyn4ik@users.noreply.github.com>
2024-09-30 17:40:10 +0300

    add controller

---
+++ TestTask/Controllers/StatsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+
+namespace TestTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatsController : ControllerBase
+    {
+        private readonly LbContext _context;
+
+        public StatsController(LbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Stats/Books
+        [HttpGet("Books")]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        // GET: api/Stats/Users
+        [HttpGet("Users")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // POST: api/Stats/AddBook
+        [HttpPost("AddBook")]
+        public async Task<ActionResult<Book>> AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetBooks", new { id = book.Id }, book);
+        }
+
+        // POST: api/Stats/AddUser
+        [HttpPost("AddUser")]
+        public async Task<ActionResult<User>> AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetUsers", new { id = user.Id }, user);
+        }
+    }
+}
--- TestTask/Data/LbContext.cs
+++ TestTask/Data/LbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Models;
 
 namespace TestTask.Data
 {
